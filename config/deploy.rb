58486a499cc97423593a148d17aa12dc192a7f2e
sharn722 <sharn722@gmail.com>
2016-03-06 20:27:31 +0530

    cap fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,13 +3,13 @@ server '104.131.2.210', port: 77, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:sharn722/wallet.git'
 set :application,     'wallet'
-set :user,            'deploy'
+#set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
 set :pty,             true
-set :use_sudo,        false
+#set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
@@ -18,7 +18,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     {  forward_agent: true, :verbose => :debug, :user => 'deploy', keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     {  forward_agent: true, :verbose => :debug, :user => 'deploy', keys: %w('~/.ssh/id_rsa.pub') }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
