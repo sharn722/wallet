7a6eb32f3f05ae0abf01c5d5f91ee404d33632f7
sharn722 win <sharn722@gmail.com>
2016-04-25 21:23:54 +0530

    cap email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,18 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => false,
+    :tls => true,
+    :ssl => true,
+    :address => 'smtp.gmail.com',
+    :port => 465,
+    :domain => 'wallet.chamalab.com',
+    :authentication => 'plain',
+    :user_name => 'sharn722@gmail.com',
+    :password => ''
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
---
+++ spec/mailers/previews/deploy_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/deploy_mailer
+class DeployMailerPreview < ActionMailer::Preview
+
+end
